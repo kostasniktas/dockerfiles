f4b01a11484a1499c4ca49c344cf911a454f286c
Kostas Niktas <kostasniktas@users.noreply.github.com>
2019-03-29 10:30:00 -0700

    shellcheck fixes plus actually timelapse it up

--- timelapse/entrypoint.sh
+++ timelapse/entrypoint.sh
@@ -2,14 +2,4 @@
 
 set -e
 
-# Default user values
-USER=${USER:=timelapsemaker}
-PASSWORD=${PASSWORD:=mysecrets}
-USERID=${USERID:=1000}
-
-# Add the user
-adduser -D -h /home/${USER} -u ${USERID} ${USER}
-chown -R ${USER} /home/${USER}
-
-echo "Command I got: ${@}"
-sudo -H -u ${USER} "${@}"
+exec "${@}"
--- timelapse/timelapsemake.sh
+++ timelapse/timelapsemake.sh
@@ -3,12 +3,28 @@
 set -e
 
 if [ "${1}" == "--help" ]; then
-  echo "Usage $(basename ${0}) [OPTIONS]"
+  SCRIPT_NAME=$(basename "${0}")
+  echo "Usage ${SCRIPT_NAME} [OPTIONS]"
   exit 1
 fi
 
-echo "I am $(whoami)"
-echo "There are $(ls -1 /input | wc -l) input files"
-echo "Testing an output touch"
-touch /output/newfile
+CURRENT=$(date +%s)
+
+echo ""
+echo Making a copy of the input images
+mkdir /output/sorted_images
+cp /input/* /output/sorted_images
+
+echo ""
+echo Renaming images
+shopt -s nocaseglob
+jhead -ntimelapse_%Y-%m-%d_%H%M%S /output/sorted_images/*.jpg
+shopt -u nocaseglob
+
+echo ""
+echo "Generating timelapse"
+ffmpeg -f image2 -r 30 -pattern_type glob -i '/output/sorted_images/*.jpg' -s hd1080 -vcodec libx264 "/output/timelapse_${CURRENT}.mp4"
+
+echo ""
+echo "Timelapse at timelapse_${CURRENT}.mp4"
 
