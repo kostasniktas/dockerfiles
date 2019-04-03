a5bc656ba01d3673471fbfb90bf67068fd1c5aeb
Kostas Niktas <kostasniktas@users.noreply.github.com>
2019-04-02 21:59:57 -0700

    Add summary files

--- timelapse/timelapsemake.sh
+++ timelapse/timelapsemake.sh
@@ -16,19 +16,36 @@ echo Making a copy of the input images
 mkdir -p "${OUTPUT}/sorted_images"
 cp /input/* "${OUTPUT}/sorted_images"
 
+echo "Original image listing" > "${OUTPUT}/original.list.txt"
+ls -1 "${OUTPUT}/sorted_images" >> "${OUTPUT}/original.list.txt"
+
 echo ""
 echo Renaming images
 shopt -s nocaseglob
-jhead -ntimelapse_%Y-%m-%d_%H%M%S "${OUTPUT}"/sorted_images/*.jpg
+jhead -ntimelapse_%Y-%m-%d_%H%M%S "${OUTPUT}"/sorted_images/*.jpg | tee "${OUTPUT}/conversion.list.txt"
 shopt -u nocaseglob
 
+echo "Sorted image listing" > "${OUTPUT}/sorted.list.txt"
+ls -1 "${OUTPUT}/sorted_images" >> "${OUTPUT}/sorted.list.txt"
+
+# shellcheck disable=SC2034
 SCALE_HD="hd1080"
 SCALE_43="1440x1080"
 
+FRAME_COUNT=$(find "${OUTPUT}/sorted_images" | wc -l)
 echo ""
-echo "Generating timelapse"
-ffmpeg -f image2 -r 30 -pattern_type glob -i "${OUTPUT}"'/sorted_images/*.jpg' -s "${SCALE_HD}" -vcodec libx264 "${OUTPUT}/timelapse_${CURRENT}.mp4"
+echo "Generating timelapse from ${FRAME_COUNT} frames"
+ffmpeg -f image2 -r 30 -pattern_type glob -i "${OUTPUT}"'/sorted_images/*.jpg' -s "${SCALE_43}" -vcodec libx264 "${OUTPUT}/timelapse_${CURRENT}.mp4"
 
 echo ""
 echo "Timelapse at ${OUTPUT}/timelapse_${CURRENT}.mp4"
 
+
+cat >"${OUTPUT}/summary.txt" <<EOF
+
+Original files: original.list.txt
+Sorted files: sorted.list.txt
+Number of images: $(find "${OUTPUT}/sorted_images" | wc -l)
+Timelapse: timelapse_${CURRENT}.mp4
+
+EOF
